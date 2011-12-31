7b3d887f29b2a46a7b33b9898bd9f5cd3b0be27f
Felix Ding <felixding@gmail.com>
2011-12-31 15:28:26 +0800

    add ENV example

---
+++ config/initializers/env.example.rb
@@ -0,0 +1,13 @@
+# This file lists all the ENV vars needed for the app
+=begin
+
+http basic auth realm
+ENV["LATTE_HBA_REALM"] = ""
+
+http basic auth username
+ENV["LATTE_HBA_USERNAME"] = ""
+
+http basic auth password
+ENV["LATTE_HBA_PASSWORD"] = ""
+
+=end
